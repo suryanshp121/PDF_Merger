cc2cbb6af191eb041fb3b99ae58f83588e1615e0
suryanshp121 <122206380+suryanshp121@users.noreply.github.com>
2024-02-05 19:58:16 +0530

    Update merger.js

--- merger.js
+++ merger.js
@@ -7,7 +7,7 @@ export const pdfmerger=async (p1,p2) => {
   await merger.add(p2); 
 
   await merger.save('public/merged.pdf'); //save under given name and reset the internal document
-  
+  merger.reset();
   // Export the merged PDF as a nodejs Buffer
   // const mergedPdfBuffer = await merger.saveAsBuffer();
   // fs.writeSync('merged.pdf', mergedPdfBuffer);
